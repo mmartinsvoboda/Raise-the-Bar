5d2e20dc92219b5cb830e62f16996a42866c4af7
Martin Svoboda <martin.svoboda@ad.is4u.cz>
2022-08-12 14:50:39 +0200

    Implemented SplashScreen API

--- app/src/main/java/com/mmartinsvoboda/sporttrackingapp/presentation/MainActivity.kt
+++ app/src/main/java/com/mmartinsvoboda/sporttrackingapp/presentation/MainActivity.kt
@@ -4,13 +4,16 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.material.MaterialTheme
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.ramcosta.composedestinations.DestinationsNavHost
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
         super.onCreate(savedInstanceState)
+
         setContent {
             MaterialTheme {
                 DestinationsNavHost(navGraph = NavGraphs.root)
