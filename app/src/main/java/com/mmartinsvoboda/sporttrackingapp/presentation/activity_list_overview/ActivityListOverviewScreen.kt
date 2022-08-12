b6ae5c682dcc15fbacc8f544ddb0a37433287bf6
Martin <martin.svoboda@ad.is4u.cz>
2022-08-12 12:05:01 +0200

    Added base for LoginScreen.kt

--- app/src/main/java/com/mmartinsvoboda/sporttrackingapp/presentation/activity_list_overview/ActivityListOverviewScreen.kt
+++ app/src/main/java/com/mmartinsvoboda/sporttrackingapp/presentation/activity_list_overview/ActivityListOverviewScreen.kt
@@ -9,10 +9,8 @@ import androidx.compose.ui.Modifier
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.mmartinsvoboda.sporttrackingapp.presentation.destinations.ActivityDetailScreenDestination
 import com.ramcosta.composedestinations.annotation.Destination
-import com.ramcosta.composedestinations.annotation.RootNavGraph
 import com.ramcosta.composedestinations.navigation.DestinationsNavigator
 
-@RootNavGraph(start = true) // sets this as the start destination of the default nav graph
 @Destination
 @Composable
 fun ActivityListOverviewScreen(
---
+++ app/src/main/java/com/mmartinsvoboda/sporttrackingapp/presentation/login/LoginScreen.kt
@@ -0,0 +1,30 @@
+package com.mmartinsvoboda.sporttrackingapp.presentation.login
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material.Button
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.mmartinsvoboda.sporttrackingapp.presentation.destinations.ActivityListOverviewScreenDestination
+import com.ramcosta.composedestinations.annotation.Destination
+import com.ramcosta.composedestinations.annotation.RootNavGraph
+import com.ramcosta.composedestinations.navigation.DestinationsNavigator
+
+@RootNavGraph(start = true) // sets this as the start destination of the default nav graph
+@Destination
+@Composable
+fun LoginScreen(
+    navigator: DestinationsNavigator, model: LoginScreenViewModel = hiltViewModel()
+) {
+    Column {
+        Text(text = "Login")
+        Button(onClick = {
+            navigator.popBackStack()
+            navigator.navigate(
+                ActivityListOverviewScreenDestination()
+            )
+        }) {
+            Text(text = "Overview")
+        }
+    }
+}
